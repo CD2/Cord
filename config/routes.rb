7079e0f67bb21f8d2c157d972d8f424b397387c5
Henry <hexmodr@gmail.com>
2017-08-30 10:39:05 +0100

    changed actions to use post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Cord::Engine.routes.draw do
   get '/*api/schema', to: 'api_base#schema'
   get '/*api/ids', to: 'api_base#ids'
-  get '/*api/perform/:action_name', to: 'api_base#perform'
+  post '/*api/perform/:action_name', to: 'api_base#perform'
   get '/*api', to: 'api_base#index'
 end
